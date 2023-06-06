ffc4eed7d59c271ee13a875c2f8c266f42c0d409
Riley Martine <rmartine@integralads.com>
2023-06-06 14:40:28 -0600

    Run prettier --write

--- plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/types.ts
+++ plugins/search-confluence-backend/src/search/ConfluenceCollatorFactory/types.ts
@@ -1,63 +1,63 @@
-import { IndexableDocument } from "@backstage/plugin-search-common";
+import { IndexableDocument } from '@backstage/plugin-search-common';
 
 export type ConfluenceDocumentMetadata = {
-    title: string;
-    status: string;
+  title: string;
+  status: string;
 
-    _links: {
-        self: string;
-        webui: string;
-    };
-}
+  _links: {
+    self: string;
+    webui: string;
+  };
+};
 
 export type ConfluenceDocumentList = {
-    results: ConfluenceDocumentMetadata[];
-    _links: {
-        next: string;
-    };
-}
+  results: ConfluenceDocumentMetadata[];
+  _links: {
+    next: string;
+  };
+};
 
 export type ConfluenceDocumentAncestorRef = {
-    title: string;
+  title: string;
 
-    _links: {
-        webui: string;
-    };
+  _links: {
+    webui: string;
+  };
 };
 
 export type ConfluenceDocument = ConfluenceDocumentMetadata & {
-    body: {
-        storage: {
-            value: string;
-        };
+  body: {
+    storage: {
+      value: string;
     };
-    version: {
-        by: {
-            publicName: string,
-        },
-        when: string;
-        friendlyWhen: string;
-    }
-    space: {
-        key: string;
-        name: string;
-        _links: {
-            webui: string;
-        };
+  };
+  version: {
+    by: {
+      publicName: string;
     };
-    ancestors: ConfluenceDocumentMetadata[];
-}
+    when: string;
+    friendlyWhen: string;
+  };
+  space: {
+    key: string;
+    name: string;
+    _links: {
+      webui: string;
+    };
+  };
+  ancestors: ConfluenceDocumentMetadata[];
+};
 
 export type IndexableAncestorRef = {
-    title: string;
-    location: string;
+  title: string;
+  location: string;
 };
 
 export type IndexableConfluenceDocument = IndexableDocument & {
-    spaceKey: string;
-    spaceName: string;
-    ancestors: IndexableAncestorRef[];
-    lastModified: string;
-    lastModifiedFriendly: string;
-    lastModifiedBy: string;
+  spaceKey: string;
+  spaceName: string;
+  ancestors: IndexableAncestorRef[];
+  lastModified: string;
+  lastModifiedFriendly: string;
+  lastModifiedBy: string;
 };
